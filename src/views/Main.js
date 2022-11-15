17bfe571752e501e8e20cb7a0dadf4949b763b65
Edwin RA <edwinra240@gmail.com>
2022-11-14 22:34:18 -0600

    Feat: Agrego react-router-dom y añado tailwindcss

---
+++ src/views/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+
+const Main = () => {
+  return (<>
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </>);
+};
+
+export default Main;
